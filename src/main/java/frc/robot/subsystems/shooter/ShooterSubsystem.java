0e40089dc264ca94ec5b5c2738ed3c1615730769
JC Baghdan <joeysamy21308@gmail.com>
2026-03-30 13:10:58 -0400

    supress unused warnings for relative encoder references not currently used

--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
+@SuppressWarnings("unused")
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
 
